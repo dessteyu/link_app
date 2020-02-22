bc4d5ef99abd9a2ef0799b2c377f154f4897a7ba
dessteyu <bayembye10@gmail.com>
2020-02-22 14:36:10 +0000

    initial commit link app

---
+++ lib/theme/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class DefaultTheme {
+  DefaultTheme._();
+  static const Color ColorPrimaryAccent = Color(0xFFA4D5ED);
+  static const Color ColorsBackgorund = Color(0xFFFAF9F5);
+  static const Color ColorPrimaryDark = Color(0xFF83BDDA);
+  static const Color ColorPrimary = Color(0xFF66B2DA);
+  static const Color ColorSecondaryAccent = Color(0xFFA9ACA8);
+  static const Color ColorSecondaryDark = Color(0xFF202A30);
+  static const Color ColorSecondary = Color(0xFF737A83);
+}
